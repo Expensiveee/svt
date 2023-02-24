ace10ce6863da029ebb899afc34deda31e808d52
Expensiveee <TheExpensiveee@gmail.com>
2023-02-25 00:02:34 +0100

    Update all.js

--- src/pages/api/categories/all.js
+++ src/pages/api/categories/all.js
@@ -3,6 +3,10 @@ import path from "path";
 
 const CATEGORIES_PATH = path.join(process.cwd(), "categories");
 
+if (!fs.access(CATEGORIES_PATH)) {
+  fs.mkdir(CATEGORIES_PATH);
+}
+
 export default async function handler(req, res) {
   const categories = await fs.readdir(CATEGORIES_PATH);
 
@@ -10,14 +14,14 @@ export default async function handler(req, res) {
   const categoriesWithMeta = await Promise.all(
     categories.map(async (category) => {
       const fullPath = path.join(CATEGORIES_PATH, category, "meta.json");
-      
+
       return await fs.readFile(fullPath, "utf8").then((data) => {
         const { name, description, slug, count } = JSON.parse(data);
         return {
           name,
           description,
           slug,
-          count
+          count,
         };
       });
     })
