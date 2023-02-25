5f67a9f689011d7d8f918243574bf7fef9a229b5
Expensiveee <TheExpensiveee@gmail.com>
2023-02-25 14:50:40 +0100

    Update [slug].js

--- src/pages/[slug].js
+++ src/pages/[slug].js
@@ -15,13 +15,13 @@ const Category = ({ data }) => {
           <S.StatisticValue color="#9b32a8">{data.meta.count}</S.StatisticValue>
         </S.Statistic>
         <S.Statistic>
-          <S.StatisticTitle>Nombre de bonnes réponses</S.StatisticTitle>
+          <S.StatisticTitle>Nombre de bonnes réponses précédentes</S.StatisticTitle>
           <S.StatisticValue color="#32a852">
             {data.statistics.correct}
           </S.StatisticValue>
         </S.Statistic>
         <S.Statistic>
-          <S.StatisticTitle>Nombre de mauvaises réponses</S.StatisticTitle>
+          <S.StatisticTitle>Nombre de mauvaises réponses précédentes</S.StatisticTitle>
           <S.StatisticValue color="#a83232">
             {data.statistics.wrong}
           </S.StatisticValue>
