6ccae819bc25348bbc46cd183e37363c89f60f86
Expensiveee <TheExpensiveee@gmail.com>
2023-02-26 22:04:28 +0100

    SEO

--- src/components/SEO/index.jsx
+++ src/components/SEO/index.jsx
@@ -9,7 +9,10 @@ export default function SEO(){
         <meta name="description" content={description} />
         <meta itemProp="name" content={title} />
         <meta itemProp="description" content={description} />
-        <meta name="theme-color" content="#ff7f00" />
+        <meta name="theme-color" content="#766ac4" />
+        <meta property="twitter:card" content="summary_large_image" />
+       <meta property="og:image" content="/dafir.jpeg" />
+       <meta property="twitter:image" content="/dafir.jpeg" />
       </>
     );
  }
