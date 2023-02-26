e1e4368e0298e54bffe8e6aba51768051a0aef9e
Expensiveee <TheExpensiveee@gmail.com>
2023-02-26 08:56:37 +0100

    Update getCategory.js

--- src/pages/api/getCategory.js
+++ src/pages/api/getCategory.js
@@ -13,8 +13,9 @@ export default async function getCategory(req, res) {
     if (!category)
       return res.status(400).json({ error: "Catégorie pas trouvé" });
 
-    // Shuffle category flashcards
-    category.flashcards.sort(() => Math.random() - 0.5);
+    // Shuffle category flashcards order and return them
+    category.flashcards = category.flashcards.sort(() => Math.random() - 0.5);
+    
     res.status(200).json(category);
   } catch (error) {
     res.status(400).json({ error });
