4113ab902da6fd107108233e54c55ee5341edd26
Expensiveee <TheExpensiveee@gmail.com>
2023-02-26 22:17:29 +0100

    Update index.jsx

--- src/components/SEO/index.jsx
+++ src/components/SEO/index.jsx
@@ -1,18 +1,28 @@
-export default function SEO(){
-    const title = "Normalement, par Ghali";
-    const description = 'Réviser c\'est bien, une personne qu t\'aime c\'est mieux. :/';
-    
-    
-    return (
-      <>
-        <title>{title}</title>
-        <meta name="description" content={description} />
-        <meta itemProp="name" content={title} />
-        <meta itemProp="description" content={description} />
-        <meta name="theme-color" content="#766ac4" />
-        <meta property="twitter:card" content="summary_large_image" />
-       <meta property="og:image" content="/dafir.jpeg" />
-       <meta property="twitter:image" content="/dafir.jpeg" />
-      </>
-    );
- }
+export default function SEO() {
+  const title = "Normalement, par Ghali";
+  const description =
+    "Réviser c'est bien, une personne qu t'aime c'est mieux. :/";
+
+  return (
+    <>
+      <title>{title}</title>
+      <meta itemProp="name" content={title} />
+      <meta name="description" content={description} />
+      <meta itemProp="description" content={description} />
+      <meta name="theme-color" content="#766ac4" />
+      <meta property="twitter:card" content="summary_large_image" />
+      <meta property="og:image" content="/dafir.jpeg" />
+      <meta property="twitter:image" content="/dafir.jpeg" />
+
+      <meta property="og:site_name" content={title} />
+      <meta property="og:title" content={title} />
+      <meta property="og:description" content={description} />
+      <meta
+        property="og:image"
+        itemprop="image"
+        content="/dafir.jpeg"
+      />
+      <meta property="og:type" content="website" />
+    </>
+  );
+}
