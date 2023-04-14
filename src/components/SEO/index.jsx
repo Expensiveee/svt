ca3cbe38f0c11e9205dc625bd7c16e3bb381e9fa
Expensiveee <TheExpensiveee@gmail.com>
2023-04-14 14:55:15 +0000

    Update index.jsx

--- src/components/SEO/index.jsx
+++ src/components/SEO/index.jsx
@@ -10,18 +10,10 @@ export default function SEO() {
       <meta name="description" content={description} />
       <meta itemProp="description" content={description} />
       <meta name="theme-color" content="#766ac4" />
-      <meta property="twitter:card" content="summary_large_image" />
-      <meta property="og:image" content="/dafir.jpeg" />
-      <meta property="twitter:image" content="/dafir.jpeg" />
 
       <meta property="og:site_name" content={title} />
       <meta property="og:title" content={title} />
       <meta property="og:description" content={description} />
-      <meta
-        property="og:image"
-        itemprop="image"
-        content="/dafir.jpeg"
-      />
       <meta property="og:type" content="website" />
     </>
   );
